508f2cda26f518452e58cd7bb05ec1039c135dd4
K Rin <rin@sandb0x.tw>
2024-10-20 07:17:24 +0000

    Modify HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+  <p class="text-2xl">Welcome to System Judge.</p>
+  <br />
+  <br />
+  <p>
+    This is an online judge system for system administration. You can test your
+    homework here to enhance your skills.
+  </p>
+  <br />
+  <p>
+    If you find this project useful, please give the github repository a star:
+    <a href="https://github.com/Rin0913/system-judge">Rin0913/system-judge</a>
+  </p>
+</template>
